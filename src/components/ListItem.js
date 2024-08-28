058a314910890988bc862ded83371f3a68266cc2
vijay1996 <vijaybhojraj.cm@gmail.com>
2024-08-29 02:09:33 +0530

    first commit

---
+++ src/components/ListItem.js
@@ -0,0 +1,23 @@
+import React, { useContext, useEffect } from "react";
+import ViewContext from "../context/ViewContext";
+
+const ListItem = ({recipe}) => {
+    const {file, name, image} = recipe;
+    const banner = require(`../assets/images/${image}`);
+
+    const {setView, setSelectedRecipeFile} = useContext(ViewContext);
+
+    const handleSelect = () => {
+        setView('recipe');
+        setSelectedRecipeFile(file);
+    }
+
+    return (
+        <div className="recipeListItem" id={file} onClick={() => handleSelect()}>
+            <img src={banner} className={'recipeListItemBaner'}/>
+            <div>{name}</div>
+        </div>
+    );
+}
+
+export default ListItem;
